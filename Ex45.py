6ce0343d21bd97e99f935c3d7898b8f5023a3162
Jhonnattan <jhonnattansantos2004@gmail.com>
2025-05-08 08:23:39 -0300

    Add files via upload

---
+++ Ex45.py
@@ -0,0 +1,7 @@
+
+nome = str(input('Informe seu nome: '))
+idade = int(input('Informe sua idade: '))
+altura = float(input('Informe sua altura: '))
+
+print('É um prazer conhecelo(a) {}, você tem {} anos e {} de altura, daqui a 5 anos você terá {} anos '.format(nome,idade,altura,idade+5))
+
